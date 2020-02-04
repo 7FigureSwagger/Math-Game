2e4e9ab8266ad691687ca0b6188bbe403a9b9bb7
7figureswagger <swistek.labs@gmail.com>
2020-02-04 15:42:44 -0800

    moved files

---
+++ classes.rb
@@ -0,0 +1,23 @@
+class Question
+  def initialize(q, a)
+    self.ques = q
+    self.ans = a
+  end
+end
+
+
+class Player
+  def initialize(n)
+    self.name = n
+  def
+end
+
+
+class Turn
+
+end 
+
+
+class Game
+
+end
---
+++ main.rb
@@ -0,0 +1,8 @@
+
+puts "Welcome to the math game"
+
+puts "Player 1 name is: "
+player1_name = gets.chomp
+
+puts "Player 2 name is: "
+player2_name = gets.chomp
